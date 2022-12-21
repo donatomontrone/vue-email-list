20a5a556ae471037f32c583f5f992f91fb3a746d
donatomontrone <donato.montrone@outlook.com>
2022-12-21 15:44:21 +0100

    add methods to create and push a randomic email

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@ const { createApp } = Vue
     createApp({
         data() {
             return {
-                randomicEmail: ''
+                randomicEmail: '',
+                emailList : [],
             }
         },
         methods : {
+            //Creo una funzione che attraverso axios e l'API di boolean genera un'email casuale e la 'pusha' in un array inizialmente vuoto
             getRandomEmail(){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then( (response) => {
-                    this.randomicEmail = response.data.response;
+                        this.randomicEmail = response.data.response;
+                        this.emailList.push(this.randomicEmail);
                 })
-            }
+            },
         },
         created(){
-            this.getRandomEmail();
-            console.log(this.randomicEmail);
         }
 }).mount('#app')
